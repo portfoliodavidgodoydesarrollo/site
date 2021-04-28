f9fd19d898fe76b521cd920f4740d10ad24508ec
agusand <amandreacchi@gmail.com>
2021-04-28 01:43:44 -0300

    Corrección estilo Titulo y Presentacion y se añaden íconos

--- src/components/Presentacion/Presentacion.js
+++ src/components/Presentacion/Presentacion.js
@@ -1,6 +1,10 @@
 import { Link } from 'react-router-dom'
 import { HashLink } from 'react-router-hash-link'
 
+import linkedin_icon from '../../assets/icons/linkedin_icon.svg'
+import github_icon from '../../assets/icons/github_icon.svg'
+import arrow_icon from '../../assets/icons/arrow_icon.svg'
+
 import "./Presentacion.css"
 
 const Presentacion = () => {
@@ -12,18 +16,21 @@ const Presentacion = () => {
                 <p className="header__saludoInicial">Me llamo David</p>
                 <p className="header__saludoInicialCopete">Soy desarrollador web, si querés un trabajo creativo
                                                         y con un código limpio, te invito a contactarme</p>
-                <button className="header__botonContacto">Contacto</button>
+                                                    
+                <button className="header__botonContacto">
+                    <HashLink className="header__botonContactoLink" smooth to="/">Contacto</HashLink>
+                </button>
                 <HashLink className="header__scrollDownLink" smooth to="/">
                     <span className="header__scrollDown">scroll down</span>
-                    <img className="header__flechaIcono" src="#" alt="scroll down"></img>
+                    <img className="header__flechaIcono" src={arrow_icon} alt="scroll down"></img>
                 </HashLink>
             </div>
             <div className="header__profesional">
-                <Link className="header__profesionalLink" to="/">
-                    <img className="header__profesionalIconos" src="#" alt="github"></img>
+                <Link to="/">
+                    <img className="header__profesionalIconos" src={github_icon} alt="github"></img>
                 </Link>
-                <Link className="header__profesionalLink" to="/">
-                    <img className="header__profesionalIconos" src="#" alt="linkedin"></img>
+                <Link to="/">
+                    <img className="header__profesionalIconos" src={linkedin_icon} alt="linkedin"></img>
                 </Link>
             </div>
         </header>
