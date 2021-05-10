9678bdda52fc3d777b7982cd34a7f83d204b7e20
agusand <amandreacchi@gmail.com>
2021-05-10 19:10:30 -0300

    Animación header

--- main.js
+++ main.js
@@ -1,41 +1,79 @@
+window.addEventListener('load', animacionPresentacion)
 
-$(document).ready(function(){
-$('#sobreMi').scroll(function(){
-    $("#sobreMi__titulosCuerpo").css("transform", "scale(0.5)")
-   $("#sobreMi__titulosCuerpo").css("transition" , "5s")
-});
-})
-
-
-function load(){
-    $("#background").css("right", "0%")
-    $("#background").css("transition", "3s")
-    
+function animacionPresentacion(){
+    const elementos = capturaElementosPresentacion()
+    animacionPresentacionFigurasGeometricas(elementos.figuras)
+    animacionPresentacionPuntos(elementos.puntos)
+    animacionPresentacionIconosProfesional(elementos.iconos)
+    elementos.puntos.addEventListener('animationend', ()=>{
+        animacionPresentacionTextoParte1(...elementos.texto1, elementos.texto2)
+        elementos.texto2.addEventListener('animationend', ()=>{
+            animacionPresentacionTextoParte2(...elementos.texto1, elementos.texto2)
+            animacionPresentacionBotonContacto(elementos.boton)
+            animacionPresentacionNombre(elementos.nombre)
+        })
+    })
 }
 
-window.onload = load;
+function capturaElementosPresentacion(){
+    return {
+        figuras : document.querySelectorAll('.background__cuerposGeometricos'),
+        puntos: document.querySelector('.background__puntos'),
+        iconos: document.querySelector('.header__profesional'),
+        texto1 : document.querySelectorAll('.header__saludoInicial'),
+        texto2: document.querySelector('.header__subSaludoInicial'),
+        boton: document.querySelector('.header__botonContacto'),
+        nombre: document.querySelector('.titulo')
+    }
+}
 
+function animacionPresentacionFigurasGeometricas(elementos){
+    elementos.forEach(elemento => {
+        elemento.style.animation = "geometrics .5s linear"
+        elemento.style.right = "18%"
+        setTimeout(()=>elemento.style.animation = "", 500)
+        
+    })
+}
 
+function animacionPresentacionPuntos(elemento){
+    elemento.style.animation = "puntos .5s linear"
+    elemento.style.top = "85%"
+}
 
-/* ScrollReveal().reveal('#sobreMi', {delay: 1000}); */
+function animacionPresentacionIconosProfesional(elemento){
+    elemento.style.animation = "iconos .5s ease-in"
+    elemento.style.opacity = "1"
+}
 
-/* 
-.sobreMi__titulosCuerpo{
-    transform: translate(50px, 50px);
-    transform: scale(0.5);
-    transition: 5s;
-    
-   } */
+function animacionPresentacionTextoParte1(...elementos){
+    elementos.forEach(elemento => {
+        elemento.style.animation = "textoparte1 .5s ease-in"
+        elemento.style.opacity = "1"
+    })
+}
 
-   //evento
+function animacionPresentacionTextoParte2(...elementos){
+    elementos.forEach(elemento => {
+        elemento.style.animation = "textoparte2 .5s ease-in"
+        elemento.style.transform = "translate(0,0) scale(1)"
+    })
+}
 
+function animacionPresentacionBotonContacto(elemento){    
+    elemento.style.animation = "boton .5s ease-in"
+    elemento.style.opacity = "1"
+}
 
-   /* $("#sobreMi").on( 'scroll', function(){
-       console.log("hola");
-   $("#sobreMi__titulosCuerpo").css("transform", "scale(0.5)")
-   $("#sobreMi__titulosCuerpo").css("transition" , "5s")
-   }); */
+function animacionPresentacionNombre(elemento){    
+    elemento.style.animation = "nombre .5s ease-in"
+    elemento.style.opacity = "1"
+}
 
-
-   
-  
+/* window.addEventListener('scroll', () => {
+    const positionY = window.scrollY
+    console.log(positionY)
+    if(positionY >= 500){
+        animacionPresentacion()
+    }
+}) */
