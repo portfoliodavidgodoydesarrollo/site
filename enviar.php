45c4e0fc567585ae0b28182f3264d37b39a40091
unknown <muotrimarcelo@gmail.com>
2021-05-13 18:06:44 +0200

    prueba correo

---
+++ enviar.php
@@ -0,0 +1,27 @@
+<!--En un archivo separado que guardarás con extensión .php y el cual se debe llamar enviar.php, pega el siguiente código:
+      
+<?php
+$name = $_POST['nombre'];
+$mail = $_POST['mail'];
+$phone = $_POST['asunto'];
+$message = $_POST['cuentame'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "El asunto es: " . $phone . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'muotrimarcelo@gmail.com';
+$asunto = 'Mensaje de PRECOMPC';
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:index.html");
+?>
+-->
